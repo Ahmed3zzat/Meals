5288bbce9bf2eb71d602b9213afbd5ad97a56091
Ahmed3zzat <ezzat.a2003@gmail.cocm>
2024-11-03 14:17:55 +0200

    first commit

---
+++ Js/main.js
@@ -0,0 +1,40 @@
+import { Home } from "./getMeals.module.js";
+let home = new Home();
+
+// start nav
+let outerLeft = $(".outer").offset().left;
+let navBar = $(".navbar");
+let navMenu = $(".navbar .outer .menu i");
+let navMenuClose = $(".navbar .outer .menu i.closeLogo");
+let listTransform = $(".inner .top-to ul li");
+navBar.css("left", -outerLeft);
+
+
+export function slideNAv(){
+  if (navBar.css("left") == "0px") {
+      navBar.animate({ left: -outerLeft }, 700);
+      listTransform.css("transform", "translateY(400px)");
+      navMenu.css("display", "block");
+      navMenuClose.css("display", "none");
+    } else {
+      navBar.animate({ left: "0px" }, 700, () => {
+        listTransform.css("transform", "translateY(0px)");
+      });
+      navMenu.css("display", "none");
+      navMenuClose.css("display", "block");
+    }
+  }
+  navMenu.click(() => {
+    slideNAv();
+  });
+// end nav
+
+// start loading
+// let loadScreen = $(".loading");
+// $(window).ready(() => {
+//   loadScreen.fadeOut(1000, () => {
+//     $("body").css({ overflow: "visible" });
+//   });
+// });
+// end loading
+
